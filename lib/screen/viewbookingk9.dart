20d54bba12a2b8d68dbfa6f57559d7ef4d91bc31
dalilanasuha <78629650+dalilanasuha@users.noreply.github.com>
2023-06-11 00:48:17 +0800

    update viewbooking untuk admin

--- lib/screen/viewbookingk9.dart
+++ lib/screen/viewbookingk9.dart
@@ -1,5 +1,6 @@
 import 'package:bukuing/screen/draweradmin.dart';
 import 'package:bukuing/screen/homeadmin.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../model/view_booking_k9_model.dart';
@@ -18,6 +19,8 @@ class _BookingHistoryWidgetState extends State<BookingK9Widget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final _unfocusNode = FocusNode();
 
+  Map users = {};
+
   @override
   void initState() {
     super.initState();
@@ -33,136 +36,123 @@ class _BookingHistoryWidgetState extends State<BookingK9Widget> {
     super.dispose();
   }
 
+  Future<List> _fetchBooking() async {
+    // Firestore initialize
+    final db = FirebaseFirestore.instance;
+    final userRef = db.collection("users");
+    users = await userRef.get().then((doc) {
+      final User = {};
+      for (var docSnapshot in doc.docs) {
+        User[docSnapshot.id] = docSnapshot.data();
+      }
+      return User;
+    }).onError((error, stackTrace) {
+      print("Error getting document: $error, $stackTrace");
+
+      return {};
+    });
+    // Fetch data from firestore
+    final colRef = db.collection("bookings");
+    final colQuery = colRef.where('court', isEqualTo: 'K9');
+    final data = await colQuery.get().then((doc) {
+      final bookings = [];
+      for (var docSnapshot in doc.docs) {
+        bookings.add({'docId': docSnapshot.id, ...docSnapshot.data()});
+      }
+      return bookings;
+    }).onError((error, stackTrace) {
+      print("Error getting document: $error, $stackTrace");
+      return [];
+    });
+
+    return data;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
-      child: Scaffold(
-        key: scaffoldKey,
-        backgroundColor: Theme.of(context).primaryColor,
-        drawer: AppDrawer(),
-        appBar: AppBar(
-          backgroundColor: Color(0xFFF7D46E),
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: Icon(
-              Icons.menu_rounded,
-              color: Theme.of(context).textTheme.bodyText1?.color,
-              size: 30,
-            ),
-            onPressed: () {
-              print('IconButton pressed ...');
-              scaffoldKey.currentState?.openDrawer();
-            },
-          ),
-          title: Text(
-            'View Booking',
-            style: TextStyle(
-              fontFamily: 'Montserrat',
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          actions: [],
-          centerTitle: true,
-          elevation: 4,
-        ),
-        body: SafeArea(
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/signinpage2.jpg"),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Stack(
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Image.asset(
-                          'images/k9k10.jpg',
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height * 0.2,
-                          fit: BoxFit.cover,
-                        ),
-                      ],
-                    ),
-                  ],
+    return FutureBuilder(
+      future: _fetchBooking(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return GestureDetector(
+            onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
+            child: Scaffold(
+              key: scaffoldKey,
+              backgroundColor: Theme.of(context).primaryColor,
+              drawer: AppDrawer(),
+              appBar: AppBar(
+                backgroundColor: Color(0xFFF7D46E),
+                automaticallyImplyLeading: false,
+                leading: IconButton(
+                  icon: Icon(
+                    Icons.menu_rounded,
+                    color: Theme.of(context).textTheme.bodyText1?.color,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    print('IconButton pressed ...');
+                    scaffoldKey.currentState?.openDrawer();
+                  },
                 ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0, 220, 0, 0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
+                title: Text(
+                  'View Booking',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                actions: [],
+                centerTitle: true,
+                elevation: 4,
+              ),
+              body: SafeArea(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/signinpage2.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Stack(
                     children: [
-                      Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Text(
-                              'K9 & K10',
-                              style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                color: Colors.white,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                                decoration: TextDecoration.underline,
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Image.asset(
+                                'images/k9k10.jpg',
+                                width: MediaQuery.of(context).size.width,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                fit: BoxFit.cover,
                               ),
-                            ),
-                          ],
-                        ),
+                            ],
+                          ),
+                        ],
                       ),
                       Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(20, 0, 10, 0),
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 220, 0, 0),
                         child: Column(
                           mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             Padding(
                               padding:
-                                  EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                                  EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0),
                               child: Row(
                                 mainAxisSize: MainAxisSize.max,
                                 children: [
                                   Text(
-                                    'addina@gmail.com',
+                                    'K9 & K10',
                                     style: TextStyle(
                                       fontFamily: 'Montserrat',
                                       color: Colors.white,
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.max,
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Text(
-                                          '5/10/23',
-                                          style: TextStyle(
-                                            fontFamily: 'Poppins',
-                                            color: Colors.white,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.normal,
-                                            fontStyle: FontStyle.normal,
-                                          ),
-                                        ),
-                                        Text(
-                                          '(10 a.m)',
-                                          style: TextStyle(
-                                            fontFamily: 'Poppins',
-                                            color: Colors.white,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.normal,
-                                            fontStyle: FontStyle.normal,
-                                          ),
-                                        ),
-                                      ],
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                      decoration: TextDecoration.underline,
                                     ),
                                   ),
                                 ],
@@ -170,42 +160,75 @@ class _BookingHistoryWidgetState extends State<BookingK9Widget> {
                             ),
                             Padding(
                               padding:
-                                  EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
-                              child: Row(
+                                  EdgeInsetsDirectional.fromSTEB(20, 0, 10, 0),
+                              child: Column(
                                 mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
-                                  Text(
-                                    'awiecomeii@gmail.com',
-                                    style: TextStyle(
-                                      fontFamily: 'Montserrat',
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.max,
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Text(
-                                          '5/10/23',
-                                          style: TextStyle(
-                                            fontFamily: 'Poppins',
-                                            color: Colors.white,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.normal,
+                                  ListView.builder(
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      shrinkWrap: true,
+                                      itemCount: snapshot.data?.length,
+                                      itemBuilder: (context, index) {
+                                        return Padding(
+                                          padding:
+                                              EdgeInsetsDirectional.fromSTEB(
+                                                  0, 20, 0, 0),
+                                          child: Row(
+                                            mainAxisSize: MainAxisSize.max,
+                                            children: [
+                                              Text(
+                                                users[snapshot.data?[index]
+                                                    ['uid']]['email'],
+                                                style: TextStyle(
+                                                  fontFamily: 'Montserrat',
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                              Expanded(
+                                                child: Row(
+                                                  mainAxisSize:
+                                                      MainAxisSize.max,
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.end,
+                                                  children: [
+                                                    Text(
+                                                      snapshot.data?[index]
+                                                          ['date'],
+                                                      style: TextStyle(
+                                                        fontFamily: 'Poppins',
+                                                        color: Colors.white,
+                                                        fontSize: 14,
+                                                        fontWeight:
+                                                            FontWeight.normal,
+                                                        fontStyle:
+                                                            FontStyle.normal,
+                                                      ),
+                                                    ),
+                                                    Text(
+                                                      '(' +
+                                                          snapshot.data?[index]
+                                                              ['start_time'] +
+                                                          ')',
+                                                      style: TextStyle(
+                                                        fontFamily: 'Poppins',
+                                                        color: Colors.white,
+                                                        fontSize: 14,
+                                                        fontWeight:
+                                                            FontWeight.normal,
+                                                        fontStyle:
+                                                            FontStyle.normal,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ],
                                           ),
-                                        ),
-                                        Text(
-                                          '(1 p.m)',
-                                          style: TextStyle(
-                                            fontFamily: 'Poppins',
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.normal,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
+                                        );
+                                      }),
                                 ],
                               ),
                             ),
@@ -215,11 +238,13 @@ class _BookingHistoryWidgetState extends State<BookingK9Widget> {
                     ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
+          );
+        } else {
+          return const LinearProgressIndicator();
+        }
+      },
     );
   }
 }
